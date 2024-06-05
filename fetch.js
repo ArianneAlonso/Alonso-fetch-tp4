8b9a06c8d2c19cb6bbd71826602332c80dc7a48e
ArianneAlonso <ariannealonso8@gmail.com>
2024-06-05 11:38:05 -0300

    se realizo el js

--- fetch.js
+++ fetch.js
@@ -1,1 +1,25 @@
-function
+const contenido = document.getElementById("contenido")
+const datosfetch = async () => {
+    try{
+        const pagina = await fetch("https://rickandmortyapi.com/api/character")
+        const dato = await pagina.json()
+        const resultado = dato.results
+
+        console.log(resultado)
+
+        resultado.forEach(pregunt => {
+            const personaje = document.createElement("div")
+            personaje.className = "foto"
+            personaje.innerHTML +=`
+           <h3>${pregunt.name}</h3>
+           <img src="${pregunt.image}" >
+           <p>${pregunt.gender}</p>
+           `
+           contenido.appendChild(item)
+        });
+    }catch(error){
+        console.error("error", error)
+    }
+}
+
+datosfetch()
